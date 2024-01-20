e72cf330bd3b1660085b4fdbe39a92fcf24e6a72
Abel Simon Zachariah <abel@Abels-MacBook-Air.local>
2024-01-20 19:10:01 +0530

    Implemented color palette

--- popup.tsx
+++ popup.tsx
@@ -1,36 +1,61 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-  const colors = ["#FF5733", "#33FF57", "#5733FF", "#FFFF33", "#33FFFF"];
+  const colorsData = require("./colors.json"); 
+  const allColors = colorsData.allColors;
+
+  const [colors, setColors] = useState(getRandomColors(allColors));
+
+  function getRandomColors(allColors) {
+    const numberOfColorsToShow = 5;
+    const shuffledColors = allColors.sort(() => 0.5 - Math.random());
+    return shuffledColors.slice(0, numberOfColorsToShow);
+  }
+
+  const handleRefresh = () => {
+    const newColors = getRandomColors(allColors);
+    setColors(newColors);
+  };
+
+  const handleCopyColorCode = (color) => {
+    const tempTextArea = document.createElement("textarea");
+    tempTextArea.value = color;
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempTextArea);
+  };
+
   return (
     <div>
-      <h2>
-        Color Palette
-      </h2>
-    <div
-      style={{
-        paddingBottom: 4
-      }}>
-      
-      {colors.map((color, index) => (
-        <div
-          key={index}
-          style={{
-            backgroundColor: color,
-            padding: 10,
-            marginBottom: 10,
-            color: "#000000",
-            width: "180px",
-            textAlign: "center"
-          }}
-        >
-          {color}
-        </div>
-      ))}
+      <h2>Color Palette</h2>
+      <div style={{ paddingBottom: 4 }}>
+        {colors.map((color, index) => (
+          <div
+            key={index}
+            style={{
+              backgroundColor: color,
+              padding: 10,
+              marginBottom: 10,
+              color: "#000000",
+              width: "180px",
+              textAlign: "center",
+              cursor: "pointer",
+            }}
+            onClick={() => handleCopyColorCode(color)}
+          >
+            {color}
+          </div>
+        ))}
+      </div>
+      <button
+        style={{ display: "block", margin: "0 auto", marginTop: "-3px" }}
+        onClick={handleRefresh}
+      >
+        Refresh
+      </button>
     </div>
-    </div>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
